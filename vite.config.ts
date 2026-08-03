3b3baba57d8790a38c1364a3ce81db97826122af
Belal Wagdy Abdelhamed <194678204+Belal08@users.noreply.github.com>
2026-08-03 09:58:31 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/Hager-Abd-Elmegid/",
-  build: { outDir: "dist", emptyOutDir: true },
-});
